e4cba95d2ed2f03f5226ab4f0ea928fa9a130af5
yanglidong <lidong.yang@pekall.com>
2021-10-25 10:25:14 +0800

    vault backup: 2021-10-25 10:25:12
    Affected files:
    .obsidian/community-plugins.json
    .obsidian/plugins/calendar/data.json
    .obsidian/plugins/calendar/main.js
    .obsidian/plugins/calendar/manifest.json
    .obsidian/plugins/cm-editor-syntax-highlight-obsidian/main.js
    .obsidian/plugins/cm-editor-syntax-highlight-obsidian/manifest.json
    .obsidian/plugins/cm-editor-syntax-highlight-obsidian/styles.css
    .obsidian/plugins/obsidian-checklist-plugin/data.json
    .obsidian/plugins/obsidian-checklist-plugin/main.js
    .obsidian/plugins/obsidian-checklist-plugin/manifest.json
    .obsidian/plugins/obsidian-checklist-plugin/styles.css
    .obsidian/plugins/obsidian-kanban/main.js
    .obsidian/plugins/obsidian-kanban/manifest.json
    .obsidian/plugins/obsidian-kanban/styles.css
    .obsidian/plugins/obsidian-mind-map/main.js
    .obsidian/plugins/obsidian-mind-map/manifest.json
    .obsidian/plugins/obsidian-outliner/main.js
    .obsidian/plugins/obsidian-outliner/manifest.json
    .obsidian/plugins/obsidian-outliner/styles.css
    .obsidian/plugins/sliding-panes-obsidian/main.js
    .obsidian/plugins/sliding-panes-obsidian/manifest.json
    .obsidian/plugins/sliding-panes-obsidian/styles.css
    .obsidian/plugins/url-into-selection/main.js
    .obsidian/plugins/url-into-selection/manifest.json
    .obsidian/workspace
    task/.DS_Store
    task/picc\347\255\226\347\225\245\346\233\264\346\226\260/edit_policy.sh
    task/picc\347\255\226\347\225\245\346\233\264\346\226\260/test_mdm_web_login.sh
    task/picc\347\255\226\347\225\245\346\233\264\346\226\260/\346\233\264\346\226\260\347\255\226\347\225\245\345\206\205\345\256\271.md
    \346\234\252\345\221\275\345\220\215.md

---
+++ task/picc策略更新/edit_policy.sh
@@ -0,0 +1,21 @@
+rm -f policy.txt
+mysql -upekall -pApple12#$ -h mgr1.mysql.pekall.com -P 6446 mdm_reactor -e "select name,description,id,com_id from mdm_new_policy_template where op_tenant_id=7" > policy.txt
+./login.sh
+cat policy.txt | while read line
+do
+ echo $line
+ arr=(`echo $line | tr '\t' ' '`)
+ echo ${arr[0]}
+ echo ${arr[1]}
+ echo ${arr[2]}
+ echo ${arr[3]}
+ body='{"name":"'${arr[0]}'","description":"'${arr[1]}'","templateId":"'${arr[2]}'"}'
+ echo $body
+ curl -k   --location --request PUT 'https://192.168.11.78:4432/adminweb/mdm/v1/new/policys/edit_policy' \
+        --header 'Nop-Tenant-Id: 7' \
+        --header 'Content-Type: application/json' \
+        --header 'Pekall-Lang: zh-CN' \
+        --cookie /tmp/cookie.txt \
+        -d '{"name":"'${arr[0]}'","description":"'${arr[1]}'","templateId":"'${arr[2]}'"}'
+done
+
